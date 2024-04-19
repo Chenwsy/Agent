10589ca5cb7d80f9ec29348d034cac9efb446273
Chenwsy <167587448+Chenwsy@users.noreply.github.com>
2024-04-20 07:45:56 +0800

    Update metadata.js

--- metadata.js
+++ metadata.js
@@ -1,3 +1,7 @@
 {
-  "name": "chenwsy"
+  "name": "Agent",
+  "website": "https://github.com/Chenwsy",
+  "description": "develop ai agent",
+  "logo": "https://raw.githubusercontent.com/Chenwsy/Agent/main/icon.png",
+  "twitter": "https://twitter.com/agent"
 }
